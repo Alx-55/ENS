d2a1fdc845843a5c46172cea13296c1e3b5195f1
Alexander <olexandrhai115@gmail.com>
2025-06-13 19:03:39 +0300

    Correction

--- main.py
+++ main.py
@@ -8,6 +8,16 @@ import httpx  # Асинхронные HTTP-запросы
 
 app = FastAPI()
 
+from database import engine
+from models import Base
+
+@app.on_event("startup")
+async def startup():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    asyncio.create_task(weather_monitor())
+
+
 # Настройки
 OPENWEATHER_API_KEY = "99e09229e133cd3639c708fe595a930b"
 
@@ -32,19 +42,26 @@ async def get_temperature(city: str):
 
 # ===== Отправка email-уведомлений =====
 async def send_notifications(city: str, temp: float):
+    async with SessionLocal() as db:
+        result = await db.execute(select(Subscriber))
+        subscribers = result.scalars().all()
+
     if not subscribers:
         print("Нет подписчиков — уведомление не отправлено.")
         return
+
+    emails = [s.email for s in subscribers]
 
     msg = EmailMessage()
     msg.set_content(f"⚠️ Внимание! В городе {city} температура {temp}°C — жарко!")
     msg["Subject"] = f"Жаркая погода в {city}"
     msg["From"] = "olexandrhai115@gmail.com"
-    msg["To"] = ", ".join(subscribers)
+    msg["To"] = ", ".join(emails)
 
     with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
-        smtp.login("olexandrhai115@gmail.com", "lftcpyhsaejlqtxk")  # пароль приложения
+        smtp.login("olexandrhai115@gmail.com", "lftcpyhsaejlqtxk")
         smtp.send_message(msg)
+
 
 
 # ===== Мониторинг погоды по городам =====
@@ -68,10 +85,29 @@ async def on_startup():
 
 
 # ===== Эндпоинт подписки =====
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from database import SessionLocal
+from models import Subscriber
+
+async def get_db():
+    async with SessionLocal() as session:
+        yield session
+
 @app.post("/subscribe")
-async def subscribe(email: str):
-    subscribers.add(email)
+async def subscribe(email: str, db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(Subscriber).where(Subscriber.email == email))
+    existing = result.scalar_one_or_none()
+
+    if existing:
+        return {"message": f"{email} уже подписан."}
+
+    new_sub = Subscriber(email=email)
+    db.add(new_sub)
+    await db.commit()
     return {"message": f"Подписка {email} добавлена"}
+
 
 
 
